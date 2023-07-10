6a618f9c92a66f1bca6faf22a56140fdee45bb61
Larissa Fitze <laerryies@gmail.com>
2023-07-10 20:56:51 +0200

    Add MB01 Lösungen

--- MB01/Program.cs
+++ MB01/Program.cs
@@ -1,3 +1,9 @@
+using Aufgabe_A1_2_1;
+using Aufgabe_A1_2_2;
+using Aufgabe_A1_2_3;
+using Aufgabe_A1_2_4;
+using Aufgabe_A1_2_5;
+
 namespace MB01
 {
     internal static class Program
@@ -11,7 +17,11 @@ namespace MB01
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new A1_2_1());
+            //Application.Run(new A1_2_2());
+            //Application.Run(new A1_2_3());
+            //Application.Run(new A1_2_4());
+            //Application.Run(new A1_2_5());
         }
     }
 }
