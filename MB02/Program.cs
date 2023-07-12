9228aa3fe89f9e74f65eef2954e70cb2ffa2117d
Larissa Fitze <laerryies@gmail.com>
2023-07-12 08:21:08 +0200

    Add MB02 Aufgaben

--- MB02/Program.cs
+++ MB02/Program.cs
@@ -1,3 +1,6 @@
+using MB02.A10_1;
+using MB02.A9_2;
+
 namespace MB02
 {
     internal static class Program
@@ -11,7 +14,24 @@ namespace MB02
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            //A9_2
+            Application.Run(new A9_2_1());
+            //Application.Run(new A9_2_2());
+            //Application.Run(new A9_2_3());
+            //Application.Run(new A9_2_4());
+            //Application.Run(new A9_2_5());
+            //Application.Run(new A9_2_6());
+
+            //A10_1
+            //Application.Run(new A10_1_1());
+            //Application.Run(new A10_1_2());
+            //Application.Run(new A10_1_3());
+            //Application.Run(new A10_1_5());
+            //Application.Run(new A10_1_6());
+            //Application.Run(new A10_1_7());
+
+
         }
     }
 }
