5c694ba3635dce973e500f2f645ebd47f30bdd71
Larissa Fitze <laerryies@gmail.com>
2024-07-19 19:30:13 +0200

    remove old

--- MB10/HashtableAufgabe/Element.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MB10.HashtableAufgabe
-{
-    public class Element
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- MB10/HashtableAufgabe/IHashtable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MB10.HashtableAufgabe
-{
-    internal interface IHashtable
-    {
-        bool Put(Element e);
-        Element Get(string id);
-        bool Delete(string id);
-    }
-}
